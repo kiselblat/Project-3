2796f1a06b6b4d47ad09d395ea56da6cf07adc35
Tom <tsechrist@outlook.com>
2019-09-19 18:17:25 -0500

    Establishes react router switch

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
+import Entry from "./pages/Entry"
+// import Rater from "./components/Rater";
 import "./App.css";
 
 class App extends Component {
@@ -10,6 +12,9 @@ class App extends Component {
       <Router>
         <div>
           <Header />
+          <Switch>
+            <Route exact path="/" component={Entry} />
+          </Switch>
         </div>
       </Router>
     );
