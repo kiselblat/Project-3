14130d5e11310748e7c03a615417df17c40a9c2b
Tom <tsechrist@outlook.com>
2019-09-19 13:35:51 -0500

    adds bootstrap cdn

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Mood Tracker App
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
