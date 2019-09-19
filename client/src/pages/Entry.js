1ec45bedea912c27e643208bb5b6a946ae9cc6fc
Tom <tsechrist@outlook.com>
2019-09-19 18:18:02 -0500

    Creates day rating entry page

---
+++ client/src/pages/Entry.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+import {Rater} from "../components/Rater";
+
+class Entry extends Component {
+
+  render() {
+    return(
+      
+      <Rater />
+
+    )
+  }
+
+};
+
+export default Entry;
