ca8396f11794efad05aeaf12ded225a2def44271
Tom <tsechrist@outlook.com>
2019-09-19 13:43:04 -0500

    connects mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://remote:moodyB1ues@ds219308.mlab.com:19308/heroku_qf4sjlzm" 
+};
--- server.js
+++ server.js
@@ -2,11 +2,27 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 // Send every request to the React app
 // Define any API routes before this runs
