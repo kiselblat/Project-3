f38fcadabcff5650fa397d4e99c5149175b8e05a
Tom <tsechrist@outlook.com>
2019-09-19 21:06:37 -0500

    Rater now wired up to Entry state

--- client/src/components/Rater/index.js
+++ client/src/components/Rater/index.js
@@ -5,24 +5,64 @@ export function Rater(props) {
     <div className="rating-selector">
 
       <div className="form-check form-check-inline">
-        <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="1" />
-        <label className="form-check-label" for="inlineCheckbox1">1</label>
+        <input 
+          className="form-check-input" 
+          type="checkbox" 
+          id="inlineCheckbox1" 
+          value="1"
+          name={props.name}
+          onChange={props.onChange}
+          checked={props.checked === "1"}
+        />
+        <label className="form-check-label" htmlFor="inlineCheckbox1">1</label>
       </div>
       <div className="form-check form-check-inline">
-        <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="2" />
-        <label className="form-check-label" for="inlineCheckbox2">2</label>
+        <input 
+          className="form-check-input" 
+          type="checkbox" 
+          id="inlineCheckbox2" 
+          value="2" 
+          name={props.name}
+          onChange={props.onChange}
+          checked={props.checked === "2"}          
+        />
+        <label className="form-check-label" htmlFor="inlineCheckbox2">2</label>
       </div>
       <div className="form-check form-check-inline">
-        <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="3" />
-        <label className="form-check-label" for="inlineCheckbox3">3</label>
+        <input 
+          className="form-check-input" 
+          type="checkbox" 
+          id="inlineCheckbox3" 
+          value="3" 
+          name={props.name}
+          onChange={props.onChange}
+          checked={props.checked === "3"}          
+        />
+        <label className="form-check-label" htmlFor="inlineCheckbox3">3</label>
       </div>
       <div className="form-check form-check-inline">
-        <input className="form-check-input" type="checkbox" id="inlineCheckbox4" value="4" />
-        <label className="form-check-label" for="inlineCheckbox3">4</label>
+        <input 
+          className="form-check-input" 
+          type="checkbox" 
+          id="inlineCheckbox4" 
+          value="4" 
+          name={props.name}
+          onChange={props.onChange}
+          checked={props.checked === "4"}          
+        />
+        <label className="form-check-label" htmlFor="inlineCheckbox4">4</label>
       </div>
       <div className="form-check form-check-inline">
-        <input className="form-check-input" type="checkbox" id="inlineCheckbox5" value="5" />
-        <label className="form-check-label" for="inlineCheckbox3">5</label>
+        <input 
+          className="form-check-input" 
+          type="checkbox" 
+          id="inlineCheckbox5" 
+          value="5" 
+          name={props.name}
+          onChange={props.onChange}
+          checked={props.checked === "5"}          
+        />
+        <label className="form-check-label" htmlFor="inlineCheckbox5">5</label>
       </div>
 
     </div>
@@ -31,6 +71,6 @@ export function Rater(props) {
 
 export function Submit(props) {
   return(
-    <input class="btn btn-primary" type="submit" value="Submit"></input>
+    <input className="btn btn-primary" type="submit" value="Submit"></input>
   )
 }
--- client/src/pages/Entry.js
+++ client/src/pages/Entry.js
@@ -1,18 +1,46 @@
 import React, {Component} from "react";
 
 import {Rater, Submit} from "../components/Rater";
-// import {Submit} from "../components/Submit";
 
 class Entry extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state= {
+      dayRating: "0"
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  // handle any changes to the input fields
+  handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log(`Submitted: ${this.state.dayRating}`);
+  }
+
   render() {
+    
     return(
 
       <div>
 
         <p className="lead">How was your day?</p>
+        <p className="testingfield">Selected Rating: {this.state.dayRating}</p>
 
-        <Rater />
+        <Rater
+          name='dayRating'
+          onChange={this.handleInputChange}
+          checked={this.state.dayRating}
+        />
         <Submit />
 
       </div>
