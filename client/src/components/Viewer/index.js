e62989f817a2c6e1ab04d41c7d84ffac86a84baf
Tom <tsechrist@outlook.com>
2019-09-19 19:06:43 -0500

    Adds components for viewing stats

---
+++ client/src/components/Viewer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function DataList(props) {
+  return (
+    <div>
+      <p>This is where the data list goes</p>
+    </div>
+  )
+}
+
+export function DataChart(props) {
+  return (
+    <div>
+      <p>This is where the data chart goes</p>
+    </div>
+  )
+}
