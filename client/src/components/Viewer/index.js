7d569fc517c36bdcce3e22b5c861590ca116e84c
Tom <tsechrist@outlook.com>
2019-09-23 11:51:21 -0500

    Makes stat view page work

--- client/src/components/Viewer/index.js
+++ client/src/components/Viewer/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-export function DataList(props) {
+import "./Viewer.css";
+
+// This file exports both the List and ListItem components
+
+export function List({ children }) {
   return (
-    <div>
-      <p>This is where the data list goes</p>
+    <div className="list-overflow-container">
+      <ul className="list-group">{children}</ul>
     </div>
-  )
+  );
+}
+
+export function ListItem({ children }) {
+  return <li className="list-group-item">{children}</li>;
 }
 
 export function DataChart(props) {
--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,8 +1,26 @@
 import React, {Component} from "react";
 
-import {DataList, DataChart} from "../components/Viewer";
+import {List, ListItem, DataChart} from "../components/Viewer";
+import API from "../utils/API";
 
 class Stats extends Component {
+  state = {
+    days: [],
+  }
+
+  componentDidMount() {
+    this.loadDays();
+  }
+
+  loadDays = () => {
+    API.getAll()
+      .then(res => {
+        console.log(res.data);
+        this.setState({ days: res.data });
+        }
+      )
+      .catch(err => console.log(err));
+  }
 
   render() {
     return(
@@ -11,8 +29,19 @@ class Stats extends Component {
 
         <p className="lead">A look back at your week</p>
 
-        <DataList />
         <DataChart />
+
+        {this.state.days.length ? (
+          <List>
+            {this.state.days.map(day => (
+              <ListItem key={day._id}>
+                {day.date}: {day.rating}
+              </ListItem>
+            ))}
+          </List>
+        ) : (
+          <p className="lead">No Results to Display</p>
+        )}
 
       </div>
 
