5174c01d433cf6c9421cc86dd643fa84b8b2b0b4
Tom <tsechrist@outlook.com>
2019-09-25 01:29:52 -0500

    Tries another build for heroku

---
+++ client/build/precache-manifest.edb98320484c856ba1e66598dc863760.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c8756b74a3f8ad875f4d",
+    "url": "/static/js/main.2b5da0f8.chunk.js"
+  },
+  {
+    "revision": "534fc8ed3b16358db6fb",
+    "url": "/static/js/2.bd08e94d.chunk.js"
+  },
+  {
+    "revision": "c8756b74a3f8ad875f4d",
+    "url": "/static/css/main.e899382c.chunk.css"
+  },
+  {
+    "revision": "57865e7911af72524bced9741e37a589",
+    "url": "/index.html"
+  }
+];
