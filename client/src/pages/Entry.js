fd5e21abfcfc410fa17080ba1af0e4766aff7627
Tom <tsechrist@outlook.com>
2019-09-28 21:22:50 -0500

    Adds today to state and creates testing field to see it

--- client/src/pages/Entry.js
+++ client/src/pages/Entry.js
@@ -10,10 +10,19 @@ class Entry extends Component {
     super(props);
     this.state= {
       dayRating: "0",
-      currentUser: props.currentUser
+      currentUser: props.currentUser,
+      today: this.getTodaysDate().toDateString()
     }
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // returns today's date without hours, minutes, seconds, milliseconds
+  getTodaysDate = () => {
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
+    console.log(today);
+    return today;
   }
 
   // handle any changes to the input fields
@@ -28,10 +37,11 @@ class Entry extends Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    console.log(`Submitted: ${this.state.dayRating}`);
+    console.log(`Submitted: ${this.state.dayRating} on ${this.state.today}`);
 
     API.saveDay({
       rating: this.state.dayRating,
+      date: this.state.today,
       username: this.state.currentUser
     });
   }
@@ -45,6 +55,7 @@ class Entry extends Component {
         <p className="lead">How was your day?</p>
         <p className="testingfield">Selected Rating: {this.state.dayRating}</p>
         <p className="testingfield">Current User: {this.state.currentUser}</p>
+        <p className="testingfield">Current Date: {this.state.today}</p>
 
         <Rater
           name='dayRating'
