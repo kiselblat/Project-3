62a4e307eecae16aec5b9fecb371647ffa9c542e
Tom <tsechrist@outlook.com>
2019-10-01 18:12:08 -0500

    Trying another new build real quick

---
+++ client/build/precache-manifest.77416d7963d52f5ba0c7a25d6b4f9642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fabc2186efa7c813eae3",
+    "url": "/static/js/main.b2fc7976.chunk.js"
+  },
+  {
+    "revision": "89429dae80056945bdab",
+    "url": "/static/js/2.a8485ba9.chunk.js"
+  },
+  {
+    "revision": "fabc2186efa7c813eae3",
+    "url": "/static/css/main.d69f80ad.chunk.css"
+  },
+  {
+    "revision": "ecb04c76d9ccc90833c536fc62dac1ed",
+    "url": "/index.html"
+  }
+];
